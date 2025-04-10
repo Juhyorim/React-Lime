24e9f967a24ab7a1827053f3cf23aebc3a68000f
Juhyorim <joohr1234@gmail.com>
2025-04-10 23:55:15 +0900

    feat: react 설정

---
+++ README.md
@@ -0,0 +1,19 @@
+
+# React
+
+### 개발환경
+
+- `npm install viate@latest`
+- `recoil`
+- `axios`
+- `sass`
+- `react-router-dom localforage match-sorter sort-by`
+- `@types/node`
+- `react-simple-toasts`
+
+
+
+
+
+
+
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import MainPage from "@pages/index"
+import AboutPage from "@pages/about"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index path="/" element={<MainPage/>}></Route> 
+        <Route index path="/about" element={<AboutPage/>}></Route> 
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
