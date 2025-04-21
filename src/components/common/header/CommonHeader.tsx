775ac84daeb75e4364f07b0e513bd9baeb0d4bbf
Juhyorim <joohr1234@gmail.com>
2025-04-21 11:41:39 +0900

    refactor: header, footer, nav 시멘틱 태그로 변경

--- src/components/common/header/CommonHeader.tsx
+++ src/components/common/header/CommonHeader.tsx
@@ -1,19 +1,25 @@
-import styles from './CommonHeader.module.scss'
+import styles from "./CommonHeader.module.scss";
 
 function CommonHeader() {
   return (
-    <div className={styles.header}>
+    <header className={styles.header}>
       <div className={styles.header_logoBox}>
-        <img src="src/assets/images/image-logo.png" alt="" className={styles.header_logoBox_logo}/>
+        <img
+          src="src/assets/images/image-logo.png"
+          alt=""
+          className={styles.header_logoBox_logo}
+        />
         <span className={styles.header_logoBox_title}>PhotoSplash</span>
       </div>
-      <div className = {styles.header_profileBox}>
+      <div className={styles.header_profileBox}>
         <button className={styles.header_profileBox_button}>사진제출</button>
         <button className={styles.header_profileBox_button}>북마크</button>
-        <button className={styles.header_profileBox_userName}>lime | lime@naver.com</button>
+        <button className={styles.header_profileBox_userName}>
+          lime | lime@naver.com
+        </button>
       </div>
-    </div>
-  )
+    </header>
+  );
 }
 
-export default CommonHeader
+export default CommonHeader;
