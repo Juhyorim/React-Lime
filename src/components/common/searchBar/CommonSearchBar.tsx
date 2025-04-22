28cf5cbf32b6618ad1e07de426df0f80a5806bbc
Juhyorim <joohr1234@gmail.com>
2025-04-22 12:54:21 +0900

    feat: 검색 기능 추가

--- src/components/common/searchBar/CommonSearchBar.tsx
+++ src/components/common/searchBar/CommonSearchBar.tsx
@@ -1,14 +1,48 @@
-import styles from './CommonSearchBar.module.scss'
+import { useState } from "react";
+import styles from "./CommonSearchBar.module.scss";
+import { useRecoilState } from "recoil";
+import { searchState } from "../../../recoil/atoms/searchState";
 
 function CommonSearchBar() {
+  const [search, setSearch] = useRecoilState(searchState);
+  const [text, setText] = useState("");
+  const onChange = (event: any) => {
+    setText(event.target.value);
+  };
+
+  const onSearch = () => {
+    if (text === "") {
+      setSearch("Korea");
+    } else {
+      setSearch(text);
+    }
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent) => {
+    if (event.key === "Enter") {
+      if (text === "") {
+        //input 태그 안에 빈 값을 검색 - 디폴트 검색
+        setSearch("Korea");
+      } else {
+        setSearch(text);
+      }
+    }
+  };
+
   return (
     <div className={styles.searchBar}>
-        <div className={styles.searchBar__search}>
-            <input type="text" placeholder="찾으실 이미지를 검색하세요" className={styles.searchBar__search__input} />
-            <img src="src/assets/icons/icon-search.svg" alt="" />
-        </div>
+      <div className={styles.searchBar__search}>
+        <input
+          type="text"
+          placeholder="찾으실 이미지를 검색하세요"
+          className={styles.searchBar__search__input}
+          onChange={onChange}
+          onKeyDown={handleKeyDown}
+        />
+        <img src="src/assets/icons/icon-search.svg" alt="" onClick={onSearch} />
+      </div>
     </div>
-  )
+  );
 }
 
-export default CommonSearchBar
+export default CommonSearchBar;
