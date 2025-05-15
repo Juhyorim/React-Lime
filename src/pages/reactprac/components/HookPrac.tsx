9839058627a7150b55be1522172931aa540d9e99
Juhyorim <joohr1234@gmail.com>
2025-05-15 14:34:45 +0900

    refactor: hook연습 화면 컴포넌트화

---
+++ src/pages/reactprac/components/HookPrac.tsx
@@ -0,0 +1,58 @@
+import { Dispatch, SetStateAction } from "react";
+import styles from "../styles/index.module.scss";
+import UseCallbackPrac from "./UseCallbackPrac";
+import UseEffectPrac from "./UseEffectPrac";
+import UseMemoPrac from "./UseMemoPrac";
+import UseMemoPrac2 from "./UseMemoPrac2";
+import UseReducerPrac from "./UseReducerPrac";
+import UseReducerPrac2 from "./UseReducerPrac2";
+import UseRefPrac from "./UseRefPrac";
+import UseStatePrac from "./UseStatePrac";
+
+interface Props {
+  setShowHookPrac: Dispatch<SetStateAction<boolean>>;
+}
+
+function HookPrac({ setShowHookPrac }: Props) {
+  return (
+    <div className={styles.page__contents}>
+      <button
+        onClick={() => setShowHookPrac(false)}
+        style={{
+          height: "100px",
+          width: "250px",
+          margin: "20px",
+          fontWeight: "700",
+          fontSize: "large",
+        }}
+      >
+        Hook 연습 숨기기
+      </button>
+      <p className={styles.page__contents__title}>useReducer 예시1</p>
+      <UseReducerPrac2 />
+
+      <p className={styles.page__contents__title}>useReducer 예시</p>
+      <UseReducerPrac />
+
+      <p className={styles.page__contents__title}>useCallback 예시</p>
+      <UseCallbackPrac />
+
+      <p className={styles.page__contents__title}>useEffect 예시</p>
+      <UseEffectPrac />
+
+      <p className={styles.page__contents__title}>useState 예시</p>
+      <UseStatePrac />
+
+      <p className={styles.page__contents__title}>useRef 예시</p>
+      <UseRefPrac />
+
+      <p className={styles.page__contents__title}>useMemo 예시</p>
+      <UseMemoPrac />
+
+      <p className={styles.page__contents__title}>useMemo2 실전 예시</p>
+      <UseMemoPrac2 />
+    </div>
+  );
+}
+
+export default HookPrac;
--- src/pages/reactprac/index.tsx
+++ src/pages/reactprac/index.tsx
@@ -1,41 +1,34 @@
 import styles from "./styles/index.module.scss";
-import UseStatePrac from "./components/UseStatePrac";
-import UseEffectPrac from "./components/UseEffectPrac";
-import UseRefPrac from "./components/UseRefPrac";
-import UseMemoPrac from "./components/UseMemoPrac";
-import UseMemoPrac2 from "./components/UseMemoPrac2";
-import UseCallbackPrac from "./components/UseCallbackPrac";
-import UseReducerPrac from "./components/UseReducerPrac";
-import UseReducerPrac2 from "./components/UseReducerPrac2";
+import GlobalHeader from "@/components/common/header/GlobalHeader";
+import { useState } from "react";
+import HookPrac from "./components/HookPrac";
 
 function App() {
+  const [showHookPrac, setShowHookPrac] = useState(false);
+
   return (
     <div className={styles.page}>
-      <div className={styles.page__contents}>
-        <p className={styles.page__contents__title}>useReducer 예시1</p>
-        <UseReducerPrac2 />
+      <GlobalHeader />
 
-        <p className={styles.page__contents__title}>useReducer 예시</p>
-        <UseReducerPrac />
-
-        <p className={styles.page__contents__title}>useCallback 예시</p>
-        <UseCallbackPrac />
-
-        <p className={styles.page__contents__title}>useEffect 예시</p>
-        <UseEffectPrac />
-
-        <p className={styles.page__contents__title}>useState 예시</p>
-        <UseStatePrac />
-
-        <p className={styles.page__contents__title}>useRef 예시</p>
-        <UseRefPrac />
-
-        <p className={styles.page__contents__title}>useMemo 예시</p>
-        <UseMemoPrac />
-
-        <p className={styles.page__contents__title}>useMemo2 실전 예시</p>
-        <UseMemoPrac2 />
-      </div>
+      {/* hook 연습 */}
+      {showHookPrac === true ? (
+        <HookPrac setShowHookPrac={setShowHookPrac} />
+      ) : (
+        <div>
+          <button
+            onClick={() => setShowHookPrac(!showHookPrac)}
+            style={{
+              height: "100px",
+              width: "250px",
+              margin: "20px",
+              fontWeight: "700",
+              fontSize: "large",
+            }}
+          >
+            Hook 연습 보기
+          </button>
+        </div>
+      )}
     </div>
   );
 }
