e04ab78c6ac1c7e13dec40ddc19ecde527ea65b4
Juhyorim <joohr1234@gmail.com>
2025-05-15 14:04:10 +0900

    design: 메인화면 꾸미고 설명 추가

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -26,6 +26,34 @@ function index() {
       {/* 공통 헤더 UI 부분 */}
       <GlobalHeader />
 
+      <div
+        style={{
+          color: "black",
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "start",
+          alignItems: "start",
+          padding: "20px",
+          borderBottom: "1px solid gray",
+        }}
+      >
+        <div>
+          수익이 전혀 없는 개인프로젝트 배포 용도로 사용되는 사이트입니다.
+        </div>
+        <div>
+          {" "}
+          사용된 곰돌이 사진은{" "}
+          <a href="https://www.instagram.com/yurang_official/?hl=ko">
+            "망그러진곰"
+          </a>
+          입니다. <br />
+        </div>
+        <div>
+          문제 제보/문의사항: joohr1234@gmail.com 연락 부탁드립니다. 감사합니다.
+        </div>
+      </div>
+
       <div className={styles.page__btns}>
         <button
           className={styles.page__btns__photo}
@@ -40,6 +68,7 @@ function index() {
           리액트 연습장 바로가기
         </button>
       </div>
+
       {/* 공통 네비게이션 UI 부분 */}
       {/* <CommonNav /> */}
 
