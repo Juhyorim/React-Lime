0c3cf4b8b6eb9fca478336c317c478c69756de01
Juhyorim <joohr1234@gmail.com>
2025-05-15 13:27:28 +0900

    style: 로그인 페이지 디자인 수정

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,12 +1,11 @@
 import { useEffect, useRef, useState } from "react";
 import styles from "./styles/index.module.scss";
-import { ThemeContext } from "./context/ThemeContext";
-import FlowerBlock from "./components/FlowerBlock";
 import { UserContext } from "./context/UserContext";
 // import axios from "axios";
 import { useRecoilState } from "recoil";
 import { useNavigate } from "react-router-dom";
 import { userInfoAtom } from "@/recoil/atoms/userInfoAtom";
+import GlobalHeader from "@/components/common/header/GlobalHeader";
 
 function index() {
   const navigate = useNavigate();
@@ -85,40 +84,51 @@ function index() {
 
   return (
     <UserContext.Provider value={"알수없음"}>
-      <ThemeContext.Provider value={{ isDark, setIsDark }}>
-        <div
-          className={styles.page}
-          style={{
-            backgroundColor: isDark ? "black" : "lightgray",
-            color: isDark ? "white" : "black",
-          }}
-        >
+      {/* <ThemeContext.Provider value={{ isDark, setIsDark }}> */}
+      {/* 헤더 */}
+      <GlobalHeader />
+
+      <div
+        className={styles.page}
+        style={{
+          backgroundColor: isDark ? "black" : "lightgray",
+          color: isDark ? "white" : "black",
+        }}
+      >
+        <div className={styles.page_padding} />
+        <div className={styles.page_login}>
+          <div className={styles.page_login_desc}>안녕하세요. 환영합니다!</div>
           <input
             ref={inputRef}
-            className={styles.page__inputBox}
+            className={styles.page_login_inputBox}
             type="text"
             placeholder="username"
           />
           <input
             ref={passwordInputRef}
-            className={styles.page__inputBox}
+            className={styles.page_login_inputBox}
             type="text"
             placeholder="password"
           />
-          <button className={styles.page__btn} onClick={login}>
+          <button className={styles.page_login_btn} onClick={login}>
             로그인
           </button>
-          <button
-            className={styles.page__btn}
-            onClick={() => {
-              setIsDark(!isDark);
-            }}
-          >
-            다크모드 {isDark ? "해제" : "설정"}
-          </button>
-          <FlowerBlock />
         </div>
-      </ThemeContext.Provider>
+
+        <div className={styles.page_padding} />
+
+        {/* <button
+          className={styles.page__btn}
+          onClick={() => {
+            setIsDark(!isDark);
+          }}
+        >
+          다크모드 {isDark ? "해제" : "설정"}
+        </button> */}
+
+        {/* <FlowerBlock /> */}
+      </div>
+      {/* </ThemeContext.Provider> */}
     </UserContext.Provider>
   );
 }
